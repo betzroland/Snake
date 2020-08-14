bb76c68826064f4a66424e01b9e8a4d45d7a1304
Roland Betz <68927907+rolandbetz@users.noreply.github.com>
2020-08-14 19:48:36 +0200

    Add files via upload

--- Headers/Draw.h
+++ Headers/Draw.h
@@ -1,24 +1,21 @@
 #ifndef DRAW_H_INCLUDED
 #define DRAW_H_INCLUDED
 
-using namespace std;
-using namespace sf;
-
-string intToString(int t);  // Auxiliary function: converts an int type to string.
+std::string intToString(int t);  // Auxiliary function: converts an int type to string.
 
 class Draw{
 public:
-    Texture texture;
-    Sprite sprite1, sprite2;
+    sf::Texture texture;
+    sf::Sprite sprite1, sprite2;
     int pixel;
 
     Draw(); // Initializing class variables.
 
-    void draw_openingwindow(RenderWindow& window);  // Draws the opening window (when the game starts).
+    void draw_openingwindow(sf::RenderWindow& window);  // Draws the opening window (when the game starts).
 
-    void draw_field(int& M, int& N, RenderWindow& window);  // Drawing the game field.
+    void draw_field(int& M, int& N, sf::RenderWindow& window);  // Drawing the game field.
 
-    void draw_gameover(RenderWindow& window, int& db, int& M, int& N); // Drawing game over window with the score.
+    void draw_gameover(sf::RenderWindow& window, int& db, int& M, int& N); // Drawing game over window with the score.
 
 };
 
--- Headers/Engine.h
+++ Headers/Engine.h
@@ -1,9 +1,6 @@
 #ifndef ENGINE_H_INCLUDED
 #define ENGINE_H_INCLUDED
 
-using namespace std;
-using namespace sf;
-
 class Snake{
     public:
     int x, y;
@@ -26,11 +23,11 @@ public:
 
     bool IsOver();  // Checks if the head of the snake has hit the walls, or its tail.
 
-    void set_startposition(Sprite& sprite2, int& pixel, RenderWindow& window);  // Setting the snake's starting position.
+    void set_startposition(sf::Sprite& sprite2, int& pixel, sf::RenderWindow& window);  // Setting the snake's starting position.
 
-    void move_control(Sprite& sprite2, int& pixel, RenderWindow& window);   // Function to control the snake.
+    void move_control(sf::Sprite& sprite2, int& pixel, sf::RenderWindow& window);   // Function to control the snake.
 
-    void fruit_generator(Sprite& sprite2, int& pixel, RenderWindow& window);    // Generating fruits randomly on the field.
+    void fruit_generator(sf::Sprite& sprite2, int& pixel, sf::RenderWindow& window);    // Generating fruits randomly on the field.
 
 };
 
