65f4a566f283ceefe1569315bb8e838493f5cb1a
Roland Betz <68927907+rolandbetz@users.noreply.github.com>
2020-08-18 18:17:43 +0200

    Add files via upload

--- Headers/Draw.h
+++ Headers/Draw.h
@@ -1,10 +1,9 @@
 #ifndef DRAW_H_INCLUDED
 #define DRAW_H_INCLUDED
 
-std::string intToString(int t);  // Auxiliary function: converts an int type to string.
-
 class Draw{
 public:
+    int M, N;
     sf::Texture texture;
     sf::Sprite sprite1, sprite2;
     int pixel;
@@ -13,9 +12,9 @@ public:
 
     void draw_openingwindow(sf::RenderWindow& window);  // Draws the opening window (when the game starts).
 
-    void draw_field(int& M, int& N, sf::RenderWindow& window);  // Drawing the game field.
+    void draw_field(sf::RenderWindow& window);  // Drawing the game field.
 
-    void draw_gameover(sf::RenderWindow& window, int& db, int& M, int& N); // Drawing game over window with the score.
+    void draw_gameover(sf::RenderWindow& window, int& db); // Drawing game over window with the score.
 
 };
 
--- Headers/Engine.h
+++ Headers/Engine.h
@@ -1,33 +1,30 @@
 #ifndef ENGINE_H_INCLUDED
 #define ENGINE_H_INCLUDED
 
-class Snake{
-    public:
+struct Snake{
     int x, y;
 };
 
-class Fruit{
-    public:
+struct Fruit{
     int x, y;
 };
 
 class Engine{
 public:
-    int M, N;
     Snake snake[100];
     int direction;
-    int db=4;
+    int db;
     Fruit fruit;
 
     Engine();   // Initializing class variables.
 
-    bool IsOver();  // Checks if the head of the snake has hit the walls, or its tail.
+    bool IsOver(int& M, int& N);  // Checks if the head of the snake has hit the walls, or its tail.
 
-    void set_startposition(sf::Sprite& sprite2, int& pixel, sf::RenderWindow& window);  // Setting the snake's starting position.
+    void set_startposition(sf::Sprite& sprite2, int& pixel, sf::RenderWindow& window, int& M, int& N);  // Setting the snake's starting position.
 
     void move_control(sf::Sprite& sprite2, int& pixel, sf::RenderWindow& window);   // Function to control the snake.
 
-    void fruit_generator(sf::Sprite& sprite2, int& pixel, sf::RenderWindow& window);    // Generating fruits randomly on the field.
+    void fruit_generator(sf::Sprite& sprite2, int& pixel, sf::RenderWindow& window, int& M, int& N);    // Generating fruits randomly on the field.
 
 };
 
